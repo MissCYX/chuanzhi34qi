e622e6fc788d9ee778d01319be5271b90d616e36
tjx12345 <tjx12345@qq.con>
2017-12-10 02:18:01 +0800

    完成打包优化

--- src/main.js
+++ src/main.js
@@ -41,19 +41,36 @@ Vue.component(MyLi.name,MyLi);
 
 // 路由相关组件 开始
 import App from './components/App.vue';
-import Home from './components/Home/Home.vue';
-import Member from './components/Member/Member.vue';
-import Shopcart from './components/Shopcart/Shopcart.vue';
-import Search from './components/Search/Search.vue';
-import NewsList from './components/News/NewsList.vue';
-import NewsDetail from './components/News/NewsDetail.vue';
-import PhotoList from './components/Photo/PhotoList.vue';
-import PhotoDetail from './components/Photo/PhotoDetail.vue';
-import GoodsList from './components/Goods/GoodsList.vue';
-import GoodsListTest from './components/Goods/GoodsList_test.vue';
-// import Comment from './components/Commons/Comment.vue';
-import GoodsDetail from './components/Goods/GoodsDetail.vue';
-import GoodsComment from './components/Goods/GoodsComment.vue';
+
+// import Home from './components/Home/Home.vue';
+// import Member from './components/Member/Member.vue';
+// import Shopcart from './components/Shopcart/Shopcart.vue';
+// import Search from './components/Search/Search.vue';
+// import NewsList from './components/News/NewsList.vue';
+// import NewsDetail from './components/News/NewsDetail.vue';
+// import PhotoList from './components/Photo/PhotoList.vue';
+// import PhotoDetail from './components/Photo/PhotoDetail.vue';
+// import GoodsList from './components/Goods/GoodsList.vue';
+// import GoodsListTest from './components/Goods/GoodsList_test.vue';
+// // import Comment from './components/Commons/Comment.vue';
+// import GoodsDetail from './components/Goods/GoodsDetail.vue';
+// import GoodsComment from './components/Goods/GoodsComment.vue';
+
+//懒加载组件
+const Home = r => require(['./components/Home/Home.vue'],r);
+const Member = r => require(['./components/Member/Member.vue'],r);
+const Shopcart = r => require(['./components/Shopcart/Shopcart.vue'],r);
+const Search = r => require(['./components/Search/Search.vue'],r);
+const NewsList = r => require(['./components/News/NewsList.vue'],r);
+const NewsDetail = r => require(['./components/News/NewsDetail.vue'],r);
+const PhotoList = r => require(['./components/Photo/PhotoList.vue'],r);
+const PhotoDetail = r => require(['./components/Photo/PhotoDetail.vue'],r);
+const GoodsList = r => require(['./components/Goods/GoodsList.vue'],r);
+const GoodsListTest = r => require(['./components/Goods/GoodsList_test.vue'],r);
+// const Comment = r => require(['./components/Commons/Comment.vue'],r);
+const GoodsDetail = r => require(['./components/Goods/GoodsDetail.vue'],r);
+const GoodsComment = r => require(['./components/Goods/GoodsComment.vue'],r);
+
 
 
 // 路由相关组件 结束
@@ -91,8 +108,46 @@ router.addRoutes([
 
 // MintUi 开始
 import MintUi from 'mint-ui';
-import 'mint-ui/lib/style.css';
-Vue.use(MintUi);
+// import 'mint-ui/lib/style.css';
+// Vue.use(MintUi);
+
+import Header from 'mint-ui/lib/header';
+import 'mint-ui/lib/header/style.css';
+import Switch from 'mint-ui/lib/switch';
+import 'mint-ui/lib/switch/style.css';
+import Swipe from 'mint-ui/lib/swipe';
+import 'mint-ui/lib/swipe/style.css';
+import SwipeItem from 'mint-ui/lib/swipe-item';
+import 'mint-ui/lib/swipe-item/style.css';
+import Lazyload from 'mint-ui/lib/lazyload';
+import 'mint-ui/lib/lazyload/style.css';
+import Loadmore from 'mint-ui/lib/loadmore';
+import 'mint-ui/lib/loadmore/style.css';
+import Indicator from 'mint-ui/lib/indicator';
+import 'mint-ui/lib/indicator/style.css';
+import Button from 'mint-ui/lib/button';
+import 'mint-ui/lib/button/style.css';
+import Toast from 'mint-ui/lib/toast';
+import 'mint-ui/lib/toast/style.css';
+import Tabbar from 'mint-ui/lib/tabbar';
+import 'mint-ui/lib/tabbar/style.css';
+import TabItem from 'mint-ui/lib/tab-item';
+import 'mint-ui/lib/tab-item/style.css';
+import Badge from 'mint-ui/lib/badge';
+import 'mint-ui/lib/badge/style.css';
+
+Vue.component(Badge.name,Badge);
+Vue.component(Tabbar.name,Tabbar);
+Vue.component(TabItem.name,TabItem);
+Vue.component(Header.name, Header);
+Vue.component(Switch.name, Switch);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+Vue.component(Loadmore.name, Loadmore);
+Vue.component(Button.name, Button);
+Vue.use(Lazyload);
+Vue.prototype.$toast = Toast;   //this.$toast('xxx')
+
 // MintUi 结束
 
 
